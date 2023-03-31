2da081b98ddfcebda76ff03b704d50253a34b2b1
junsobi <127650045+junsobi@users.noreply.github.com>
2023-03-31 22:58:08 +0900

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,66 +1,85 @@
+let touchStartX;
+let touchStartY;
+
 document.addEventListener("keydown", function (event) {
   moveHero(event.key);
-});
-document.addEventListener("touchmove", function(event) {
-  event.preventDefault();
-});
-document.addEventListener("touchstart", function (event) {
-  const touchX = event.changedTouches[0].clientX;
-  const touchY = event.changedTouches[0].clientY;
-  const heroX = heroCond.getBoundingClientRect().left;
-  const heroY = heroCond.getBoundingClientRect().top;
-
-  if (touchX > heroX) {
-    moveHero("ArrowRight");
-  } else if (touchX < heroX) {
-    moveHero("ArrowLeft");
-  } else if (touchY < heroY) {
-    moveHero("ArrowUp");
-  } else if (touchY > heroY) {
-    moveHero("ArrowDown");
-  }
 });
 
 document.addEventListener("keyup", function () {
   heroCond.className = "stop";
 });
 
+document.addEventListener(
+  "touchstart",
+  function (event) {
+    event.preventDefault();
+    touchStartX = event.touches[0].clientX;
+    touchStartY = event.touches[0].clientY;
+    heroCond.className = "stop";
+  },
+  { passive: false }
+);
+
+document.addEventListener(
+  "touchmove",
+  function (event) {
+    if (!event.cancelable) {
+      return;
+    }
+
+    const touchEndX = event.changedTouches[0].clientX;
+    const touchEndY = event.changedTouches[0].clientY;
+
+    const touchDistanceX = touchEndX - touchStartX;
+    const touchDistanceY = touchEndY - touchStartY;
+
+    if (Math.abs(touchDistanceX) > Math.abs(touchDistanceY)) {
+      if (touchDistanceX > 0) {
+        moveHero("ArrowRight");
+      } else {
+        moveHero("ArrowLeft");
+      }
+    } else {
+      if (touchDistanceY < 0) {
+        moveHero("ArrowUp");
+      } else {
+        moveHero("ArrowDown");
+      }
+    }
+  },
+  { passive: false }
+);
+
 function moveHero(key) {
   const currentLeft = getComputedStyle(heroCond).left;
   const currentBottom = getComputedStyle(heroCond).bottom;
 
-  const numLeft = Number(currentLeft.split("px")[0]); //px를 뺀 나머지 숫자 레프트
-  const numBottom = Number(currentBottom.split("px")[0]); //px를 뺀 나머지 숫자 바텀
+  const numLeft = Number(currentLeft.split("px")[0]);
+  const numBottom = Number(currentBottom.split("px")[0]);
   const bgWidth = bgCond.clientWidth;
   const heroWidth = heroCond.clientWidth;
   const bgHeight = bgCond.clientHeight;
   const heroHeight = heroCond.clientHeight;
 
-  //  hero. stop right back이 있다
-  //키코드 39:우측 37좌측 38위쪽 40 아래쪽
   if (key === "ArrowLeft") {
     heroCond.className = "left";
     if (numLeft > 10) {
       heroCond.style.left = numLeft - 55 + "px";
     }
-  } //왼쪽키가 눌렸을때 해야할일 : 현재 위치 -1px
-  else if (key === "ArrowRight") {
+  } else if (key === "ArrowRight") {
     heroCond.className = "right";
     if (numLeft < bgWidth - heroWidth - 25) {
       heroCond.style.left = numLeft + 55 + "px";
     }
-  } //우측키가 눌렸을때 해야할일
-  else if (key === "ArrowUp") {
+  } else if (key === "ArrowUp") {
     if (numBottom < bgHeight - heroHeight - 30) {
       heroCond.className = "back";
       heroCond.style.bottom = numBottom + 55 + "px";
     }
-  } //위쪽키가 눌렸을때 해야할일
-  else if (key === "ArrowDown") {
+  } else if (key === "ArrowDown") {
     heroCond.className = "stop";
     if (numBottom > 0) {
       heroCond.style.bottom = numBottom - 55 + "px";
     }
-    //아래키가 눌렸을때 해야할일
   }
 }
